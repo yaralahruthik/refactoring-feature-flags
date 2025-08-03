a7cb5e5a4e8cbc395a9355bc23f6fc8a3c9e0efb
yaralahruthik <yaralahruthikreddy@gmail.com>
2025-08-03 20:36:40 +0530

    mantine setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,26 @@ import About from '@/pages/about';
 import Home from '@/pages/home';
 import Login from '@/pages/login';
 import Register from '@/pages/register';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 import { Route, Routes } from 'react-router';
 import AppLayout from './layouts/app-layout';
 
 function App() {
   return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Route>
+    <MantineProvider>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
 
-      <Route element={<AuthLayout />}>
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-      </Route>
-    </Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </MantineProvider>
   );
 }
 
